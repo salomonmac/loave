b920089d5980f8a0460e6129f1427ce3c0827a05
Saul Salomon Ramirez Zuñiga <geforcesalomonti@gmail.com>
2017-07-28 14:21:34 -0500

    anexo de scroll

--- js/menu.js
+++ js/menu.js
@@ -10,8 +10,10 @@ $(document).ready(main);
                            left: '0'
                        });
                        contador = 0;
+                       $("#parar").addClass("parar");
                    } else {
                        contador = 1;
+                        $("#parar").removeClass("parar");
                        $('.menu-wsi').animate({
                            left: '-100%'
                        });
@@ -20,73 +22,3 @@ $(document).ready(main);
 
 
     };
-               $('.menu_compartir').click(function(){
-                   // $('nav').toggle();
-
-                   if(contador == 1){
-                       $('.compartir').animate({
-                           left: '0'
-                       });
-                       contador = 0;
-                   } else {
-                       contador = 1;
-                       $('.compartir').animate({
-                           left: '-100%'
-                       });
-                   }
-               });
-
-
-                $('.menu_buscar').click(function(){
-                   // $('nav').toggle();
-
-                   if(contador == 1){
-                       $('.buscar').animate({
-                           left: '0'
-                       });
-                       contador = 0;
-                   } else {
-                       contador = 1;
-                       $('.buscar').animate({
-                           left: '-100%'
-                       });
-                   }
-               });
-
-
-               $('.menu_traduccion').click(function(){
-                   // $('nav').toggle();
-
-                   if(contador == 1){
-                       $('.traduccion').animate({
-                           left: '0'
-                       });
-                       contador = 0;
-                   } else {
-                       contador = 1;
-                       $('.traduccion').animate({
-                           left: '-100%'
-                       });
-                   }
-               });
-
-
-               $('.xxx').click(function(){
-                   // $('nav').toggle();
-
-                   if(contador == 1){
-                       $('.traduccion').animate({
-                           left: '-100%'
-                       });
-                       $('.buscar').animate({
-                           left: '-100%'
-                       });
-                       $('.compartir').animate({
-                           left: '-100%'
-                       });
-                       $('.menu-wsi').animate({
-                           left: '-100%'
-                       });
-                       contador = 0;
-                   }
-               });
